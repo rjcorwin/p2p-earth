49446be9cf9d157d73df1c4902b3a420a509138d
R.J. Steinert <rj@rjsteinert.com>
2017-12-26 16:44:58 +0000

    Remove auth0, replace with google auth, get /users/me route on the rails

--- server/app.js
+++ server/app.js
@@ -4,42 +4,16 @@ PouchDB.defaults({prefix: './db/'})
 
 var express = require('express')
 var passport = require('passport')
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 var path = require('path')
-
-//var User = require('./User')
-class Users {
-  constructor() {
-    this.db = new PouchDB('users')
-  }
-  async findOrCreate(profile, done) {
-    try {
-      let user = await this.db.get(profile.id)
-      done(null, profile)
-    } catch(err) {
-      console.log(`Creating user ${profile.id}`)
-      console.log(JSON.stringify(profile))
-      try {
-        let res = await this.db.put(Object.assign({_id: profile.id}, profile))
-      } catch(err) {
-        console.log(err)
-      }
-      console.log(res)
-      //let user = await this.db.get(res.id)
-      //return user
-      done(null, profile)
-    }
-  }
-}
-
-var users = new Users() 
 
 //var config = read.sync('/tangerine/server/config.yml')
 
 var app = express()
 
 app.use(function (req, res, next) {
-  console.log('Time:', Date.now())
-  console.log(Object.keys(req))
+  //console.log('Time:', Date.now())
+  //console.log(Object.keys(req))
   next()
 })
 //app.use(express.static('public'));
@@ -58,6 +32,48 @@ app.use(require('express-session')({ secret: 'foo', resave: true, saveUninitiali
 app.use(passport.initialize());
 app.use(passport.session());
 
+//var User = require('./User')
+class Users {
+  constructor() {
+    this.db = new PouchDB('users')
+  }
+  async findById(id) {
+    try {
+      let user = await this.db.get(id)
+      return user
+    } catch (e) {
+      console.log(e)
+    }
+    return {}
+  }
+
+  async findOrCreate(profile, done) {
+    try {
+      let user = await this.db.get(profile.id)
+      done(null, profile)
+    } catch(err) {
+      console.log(`Creating user ${profile.id}`)
+      console.log(JSON.stringify(profile))
+      try {
+        let userObject = Object.assign({_id: profile.id}, profile) 
+        delete userObject._raw
+        delete userObject._json
+        let res = await this.db.post(userObject)
+        console.log(res)
+      } catch(e) {
+        console.log('ERROR: Could not save user.')
+        console.log(e)
+      }
+      //let user = await this.db.get(res.id)
+      //return user
+      done(null, profile)
+    }
+  }
+}
+
+var users = new Users() 
+
+
 passport.serializeUser(function(user, done) {
   console.log(user)
   done(null, user);
@@ -69,47 +85,64 @@ passport.deserializeUser(function(user, done) {
 });
 
 /*
- * Auth0 Auth
+app.get('/users/:id', async function(req, res) {
+  let user = await users.findById(req.param.id)
+  return res.json(user)
+})
+*/
+ 
+app.get('/users/me', async function(req, res) {
+  if (req.user && req.user.id) {
+    let user = await users.findById(req.user.id)
+    return res.json(user)
+  } else {
+    return res.json({})
+  }
+
+})
+  
+ 
+
+/*
+ * Google Auth
  */
-var Auth0Strategy = require('passport-auth0')//,
-//    passport = require('passport');
-console.log(process.env.BASE_PATH)
-var strategy = new Auth0Strategy({
-   domain:       process.env.AUTH0_DOMAIN,
-   clientID:     process.env.AUTH0_CLIENT_ID,
-   clientSecret: process.env.AUTH0_CLIENT_SECRET,
-   callbackURL:  process.env.BASE_PATH + '/auth0/callback'
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
+const GOOGLE_AUTH_CALLBACK_URL = process.env.BASE_PATH + "/auth/google/callback"
+// Use the GoogleStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Google
+//   profile), and invoke a callback with a user object.
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: GOOGLE_AUTH_CALLBACK_URL
   },
-  function(accessToken, refreshToken, extraParams, profile, done) {
-    // accessToken is the token to call Auth0 API (not needed in the most cases)
-    // extraParams.id_token has the JSON Web Token
-    // profile has all the information from the user
-    /*
-    users.findOrCreate(profile, function (err, user) {
-      return done(err, user)
-    })
-    */
-    return done(null, profile)
+  function(accessToken, refreshToken, profile, done) {
+     users.findOrCreate(profile, function (err, user) {
+       return done(err, user)
+     })
+     // return done(null, { name: "test", id: profile.id, profile: profile })
   }
-);
+))
 
-passport.use(strategy);
+// GET /auth/google
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Google authentication will involve
+//   redirecting the user to google.com.  After authorization, Google
+//   will redirect the user back to this application at /auth/google/callback
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }))
 
-app.get('/auth0/callback',
-  passport.authenticate('auth0', { failureRedirect: '/login' }),
+// GET /auth/google/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
-    console.log('LOGIN')
-    console.log(JSON.stringify(req.user))
-    if (!req.user) {
-      throw new Error('user null');
-    }
-    res.redirect("/");
-  }
-);
-
-app.get('/auth0/login',
-  passport.authenticate('auth0', {}), function (req, res) {
-  res.redirect("/");
-});
+    res.redirect('/')
+  })
 
 app.listen(3000)
